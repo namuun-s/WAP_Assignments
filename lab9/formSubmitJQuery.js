355a09d67cf481229c80cd8b20bc1ff599c6aa48
Namuun <s.namuun88@gmail.com>
2020-04-01 15:51:13 -0500

    Lab 9

--- lab9/formSubmitJQuery.js
+++ lab9/formSubmitJQuery.js
@@ -7,18 +7,23 @@ $(function(){
     $('form').submit(function(e){
         e.preventDefault();  
         const formId = $(this).attr("id");
-        let newElement;
+        //I'll use this newElement to show the ProductForm data on the page.
+        let newElement=null;
         if(formId==="productForm"){
+            //If the form is Product Form, then I'll create a new DOM element (div);
             newElement = $("<div>", { "id": "newDiv", "class": "special" });
         }
         jQuery.each($( this ).serializeArray(), function(index, item) {
             console.log(item.name+ ": " + item.value);  
             if(formId==="productForm"){
+                //If the form is Product Form, then I'll create a HTML paragraph (p tag) for each data input.
                 let p = $("<p>",{"text":item.name+ ": " + item.value});
+                //Then I'll add paragraph to the above div (newElement);
                 newElement.append(p);
             }
         });
-        $('#result').append(newElement);
+        //Finally, if newElement is not null, then I'll append it to the ProductForm's page's HTML.
+        if(newElement!=null) $('#result').append(newElement);
     });
 });
 
